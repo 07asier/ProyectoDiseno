8c79b0873ba4839d06008c4e6f3d92fc51f8e11b
“07asier” <asier2223@gmail.com>
2017-11-14 12:17:45 +0100

    Initial commit

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Proyecto Diseño</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <link href="css/estilosinicio.css" rel="stylesheet">
+
+
+</head>
+
+<body >
+
+<!-- Navigation -->
+<ul>
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="#contact">Contacto</a></li>
+    <li><a href="#about">Registro/Login</a></li>
+</ul>
+
+<header class="masthead text-center text-white d-flex">
+    <div class="container my-auto">
+        <div class="row">
+            <div class="cuadro">
+                <h1 class="text-uppercase">
+                    <p>Fabrica Nº1 en</p>
+                    <p> piezas de competición</p>
+                </h1>
+
+            </div>
+
+        </div>
+    </div>
+</header>
+
+
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Apariciones</h2>
+                <hr class="my-4">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <p></p>
+        </div>
+    </div>
+    <hr class="my-4">
+</section>
+
+
+</body>
+
+
+</html>
